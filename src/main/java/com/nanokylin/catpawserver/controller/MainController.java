135d87f8bbcc8e75efd3ca9609f2b02ecb0d4042
Hanbings <3219065882@qq.com>
2020-09-20 22:39:51 +0800

    :wrench:​ Fix: Log4j2.xml changed

--- src/main/java/com/nanokylin/catpawserver/controller/MainController.java
+++ src/main/java/com/nanokylin/catpawserver/controller/MainController.java
@@ -1,9 +1,16 @@
 package com.nanokylin.catpawserver.controller;
 
 import com.nanokylin.catpawserver.common.BaseInfo;
+import com.nanokylin.catpawserver.util.LogUtil;
 
 public class MainController {
     public void RunCatPawServer(){
-        System.out.println(BaseInfo.CAT_PAW_SERVER_LOGO);
+        LogUtil log = new LogUtil();
+        log.info("操作系统: " + BaseInfo.SYSTEM_NAME + " " + BaseInfo.SYSTEM_VERSION);
+        log.info("系统构架: " + BaseInfo.SYSTEM_ARCH);
+        log.info("Java版本: " + BaseInfo.JAVA_VM_VERSION);
+        log.info("CatPawServer版本: " + BaseInfo.CAT_PAW_SERVER_VERSION);
+        log.info("CatPawServer构建时间: " + BaseInfo.CAT_PAW_SERVER_BUILD_TIME);
+        log.info(BaseInfo.CAT_PAW_SERVER_LOGO);
     }
 }
