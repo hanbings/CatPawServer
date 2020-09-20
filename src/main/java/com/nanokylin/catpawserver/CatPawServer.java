ed3c182639a5d4d74716848d55eef7a95ccafef7
Hanbings <3219065882@qq.com>
2020-09-20 22:23:21 +0800

    :tada:​ Style: Init Project

---
+++ src/main/java/com/nanokylin/catpawserver/CatPawServer.java
@@ -0,0 +1,13 @@
+package com.nanokylin.catpawserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class CatPawServer
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/nanokylin/catpawserver/CatPawServerTest.java
@@ -0,0 +1,20 @@
+package com.nanokylin.catpawserver;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CatPawServerTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
