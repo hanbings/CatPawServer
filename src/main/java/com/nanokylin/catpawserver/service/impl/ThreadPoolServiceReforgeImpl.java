6d782f2817f6d531ba6d66cb40296f8abf268902
Hanbings <3219065882@qq.com>
2020-10-20 09:54:33 +0800

    :construction:​ Refact: Unknow

---
+++ src/main/java/com/nanokylin/catpawserver/service/impl/ThreadPoolServiceReforgeImpl.java
@@ -0,0 +1,28 @@
+package com.nanokylin.catpawserver.service.impl;
+
+import com.nanokylin.catpawserver.service.ThreadPoolService;
+
+public class ThreadPoolServiceReforgeImpl implements ThreadPoolService {
+    ThreadPoolServiceReforgeImpl(){
+        
+    }
+    @Override
+    public void initThreadPoolService() {
+
+    }
+
+    @Override
+    public void execute(Thread thread) {
+
+    }
+
+    @Override
+    public void shutdown(String reason) {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
